066607a9a816efd757f8087e63ed3463927a8fce
FayeHardie <faye.hardie01@outlook.com>
2024-11-27 09:55:44 +0000

    added more to species, and changed font size for form title

---
+++ shark webpage/gallery.js
@@ -0,0 +1,10 @@
+//NavBar sidebar
+function showSidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
--- shark webpage/species.js
+++ shark webpage/species.js
@@ -9,35 +9,6 @@ function hideSidebar(){
     sidebar.style.display = "none";
 }
 
-//Photo sliderv1
-/*
-const slides = document.querySelector('.species');
-const images = [
-    '1.png',
-    '2.png',
-    '3.png',
-    '4.png',
-    '5.png'
-]
-
-let i = 0;
-
-function prevSlide(){
-    if (i <= 0) i = images.length;
-    i--;
-    return setImg();
-}
-
-function nextSlide(){
-    if (i >= images.length-1) i = -1;
-    i++;
-    return setImg();
-}
-
-function setImg(){
-    return slides.setAttribute('src', '/photoslider/' + images[i]);
-}
-*/
 // Photo slider with text
 
 // set slide index to default 0
